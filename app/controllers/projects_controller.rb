35e09d739cc0c656843cb76ce30b1ab0ee9e6d41
Nevena Djaja <nevena.djaja@gmail.com>
2016-03-14 13:32:27 -0700

    added update route

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,4 +6,16 @@ class ProjectsController < ApplicationController
   def show
   	@project = Project.find(params[:id])
   end
+
+  def edit
+  	@project = Project.find(params[:id])
+  end
+
+  def update
+  	@project = Project.find(params[:id])
+  	project_params = params.require(:project).
+  										permit(:name, :description, :target_pledge_amount, :website, :pledging_ends_on)
+  	@project.update(project_params)
+  	redirect_to project_path(@project)
+  end
 end
