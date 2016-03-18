42b7fb0dc7c9668574269d864fdf170155af121e
Nevena Djaja <nevena.djaja@gmail.com>
2016-03-17 18:59:05 -0700

    filter only active projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ActiveRecord::Base
 	def pledging_done?
 		pledging_ends_on < Date.today
 	end
+
+	def self.active
+		where("pledging_ends_on >= ?", Time.now).order("pledging_ends_on")
+	end
 end
