be5171237cacae77188f5d64b257428c112bfa79
Nevena Djaja <nevena.djaja@gmail.com>
2016-03-23 11:12:37 -0700

    added validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,21 @@
 class Project < ActiveRecord::Base
+
+	validates :name, :description, presence: true
+
+	validates :description, length: { maximum: 500 }
+
+	validates :target_pledge_amount, numericality: { greater_than: 0 }
+
+	validates :website, allow_blank: true, format: {
+		with: /https?:\/\/[\S]+\b/i,
+		message: 'must have a valid URI'
+	}
+
+	validates :image_file_name, format: {
+    with: /\w+\.(gif|jpg|png)\z/i,
+    message: "must reference a GIF, JPG, or PNG image"
+  }
+
 	def pledging_done?
 		pledging_ends_on < Date.today
 	end
