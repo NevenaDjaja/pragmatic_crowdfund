763643a22ac1583bae95902779eb7e55c3a499ec
Nevena Djaja <nevena.djaja@gmail.com>
2016-03-22 20:11:13 -0700

    updated styles

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,17 +1,17 @@
 module ProjectsHelper
-	def time_from_now(project)
-		if project.pledging_done?
-			content_tag(:strong, "All Done!")
-		else
-			time_ago_in_words(project[:pledging_ends_on]) + " remaining"
-		end
-	end
+  def format_pledging_ends_on(project)
+    if project.pledging_done?
+      content_tag(:strong, 'All Done!')
+    else
+      distance_of_time_in_words(Date.today, project.pledging_ends_on) + " remaining"
+    end
+  end
 
-	def image_for(project)
-		if project.image_file_name.blank?
-			image_tag 'placeholder.png'
-		else
-			image_tag project.image_file_name
-		end
-	end
+  def image_for(project)
+    if project.image_file_name.blank?
+      image_tag 'placeholder.png'
+    else
+      image_tag project.image_file_name
+    end
+  end
 end
